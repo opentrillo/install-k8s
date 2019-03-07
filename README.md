9f994ce5ef5f680c9e3b6732211eedede259ab9c
Saqib Awan <saawan@trill.io>
2019-03-06 21:18:39 -0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # install-k8s
-Install trillo runtime on k8s
+installation script for any k8s cluster
---
+++ deploy-trillo.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+#set the config straight below, if running from cmd-line
+#gcloud container clusters get-credentials trillo-rt-k8s --zone us-central1-a --project project-trillort
+
+kubectl apply -f namespace.yaml
+kubectl apply -f secrets-dh.yaml
+kubectl apply -f secrets-mysql.yaml
+kubectl apply -f pvpvc-stage1.yaml
+kubectl apply -f deploy-nfs.yaml
+kubectl apply -f pvpvc-stage2.yaml
+kubectl apply -f deploy-mysql.yaml
+kubectl apply -f deploy-ds.yaml
+kubectl apply -f deploy-rt.yaml
+kubectl apply -f rt-service-account.yaml
+
